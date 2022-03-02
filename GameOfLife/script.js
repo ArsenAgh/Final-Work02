1692bd105f1c00ca0eef9005762cb50ad8078b4b
ArsenAgh <arsen.aghajanyan@tumo.org>
2022-03-02 17:57:26 +0300

    mnac 1 kerpar

---
+++ GameOfLife/script.js
@@ -0,0 +1,143 @@
+function generator(matLen, gr, grEat, pred, virus, energy, zombie) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+
+    }
+    for (let i = 0; i < pred; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < virus; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+    for (let i = 0; i < energy; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+    }
+    for (let i = 0; i < zombie; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 6;
+        }
+    }
+    return matrix;
+}
+
+let side = 20;
+
+let matrix = generator(50, 350, 200, 125, 90, 200, 150);
+
+let grassArr = []
+let grassEaterArr = []
+let predatorArr = []
+let virusArr = []
+let energyArr = []
+let zombieArr = []
+function setup() {
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+    frameRate(3)
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let gr = new Grass(x, y)
+                grassArr.push(gr)
+            } else if (matrix[y][x] == 2) {
+                let grE = new GrassEater(x, y)
+                grassEaterArr.push(grE)
+            }
+            else if (matrix[y][x] == 3) {
+                let pr = new Predator(x, y)
+                predatorArr.push(pr)
+            }
+            else if (matrix[y][x] == 4) {
+                let vs = new Virus(x, y)
+                virusArr.push(vs)
+            }
+            else if (matrix[y][x] == 5) {
+                let en = new Energy(x, y)
+                energyArr.push(en)
+            }
+            else if (matrix[y][x] == 6) {
+                let zb = new Zombie(x, y)
+                zombieArr.push(zb)
+            }
+        }
+    }
+}
+
+
+
+function draw() {
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                fill('green')
+            } else if (matrix[y][x] == 0) {
+                fill('#acacac')
+            } else if (matrix[y][x] == 2) {
+                fill('yellow')
+            }
+            else if (matrix[y][x] == 3) {
+                fill('red')
+            }
+            else if (matrix[y][x] == 4) {
+                fill('blue')
+            }
+            else if (matrix[y][x] == 5) {
+                fill('purple')
+            }
+            else if (matrix[y][x] == 6) {
+                fill('brown')
+            }
+            rect(x * side, y * side, side, side)
+        }
+    }
+
+
+    for (let i in grassArr) {
+        grassArr[i].mul()
+    }
+    for (let i in grassEaterArr) {
+        grassEaterArr[i].mul()
+        grassEaterArr[i].eat()
+    }
+    for (let i in predatorArr) {
+        predatorArr[i].mul()
+        predatorArr[i].eat()
+    }
+    for (let i in zombieArr) {
+        zombieArr[i].mul()
+        zombieArr[i].eat()
+        zombieArr[i].move()
+    }
+}
---
+++ GameOfLife/zombie.js
@@ -0,0 +1,102 @@
+class Zombie extends LivingCreature {
+
+  
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(ch) {
+        this.getNewCoordinates();
+        return super.chooseCell(ch);
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        if (newCell && this.multiply >= 25) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 3;
+
+            var newZb = new Zombie(newX, newY);
+            zombieArr.push(newZb);
+            this.multiply = 0;
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+        if (newCell && this.energy >= 0) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        } else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(4);
+        var newCell = random(emptyCells);
+        var emptyCells2 = this.chooseCell(5);
+        var newCell2 = random(emptyCells2);
+        if (newCell) {
+            this.energy+5
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in virusArr) {
+                if (newX == virusArr[i].x && newY == virusArr[i].y) {
+                    virusArr.splice(i, 1);
+                    break;
+                }
+            }
+            
+           
+        } 
+        else if (newCell2) {
+            this.energy += 4
+            var newX = newCell2[0];
+            var newY = newCell2[1];
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in energyArr) {
+                if (newX == energyArr[i].x && newY == energyArr[i].y) {
+                    energyArr.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+    die() {
+        matrix[this.y][this.x] = 0
+        for (var i in zombieArr) {
+            if (this.x == zombieArr[i].x && this.y == zombieArr[i].y) {
+                zombieArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
